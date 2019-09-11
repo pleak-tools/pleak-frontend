0c4bb8d827aac0235cf1377f5c8e4e27abcfa227
Aivo Toots <aivo.toots@cyber.ee>
2019-09-11 15:34:40 +0300

    Support for the latest SQL analysis functionalities

--- src/assets/bpmn-labels-extension.ts
+++ src/assets/bpmn-labels-extension.ts
@@ -3,314 +3,319 @@ export const SqlBPMNModdle = {
   prefix: 'pleak',
   uri: 'http://pleak.io/',
   xml: {
-    tagAlias: 'lowerCase'
+    tagAlias: "lowerCase"
   },
-  associations: [],
+  associations: new Array(),
   types: [
     {
-      name: 'SQLTask',
+      name: "SQLTask",
       extends: [
-        'bpmn:Task',
-        'bpmn:StartEvent',
-        'bpmn:IntermediateCatchEvent'
+        "bpmn:Task",
+        "bpmn:StartEvent",
+        "bpmn:IntermediateCatchEvent"
       ],
       properties: [
         {
-          'name': 'sqlScript',
-          'isAttr': false,
-          'type': 'String'
+          "name": "sqlScript",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'sensitivityMatrix',
-          'isAttr': false,
-          'type': 'String'
+          "name": "sensitivityMatrix",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'Policies',
+      name: "Policies",
       extends: [
-        'bpmn:Participant'
+        "bpmn:Participant"
       ],
       properties: [
         {
-          'name': 'policyScript',
-          'isAttr': false,
-          'type': 'String'
+          "name": "policyScript",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'StereotypeTask',
+      name: "StereotypeTask",
       extends: [
-        'bpmn:Task'
+        "bpmn:Task"
       ],
       properties: [
         {
-          'name': 'PKEncrypt',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PKEncrypt",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'PKDecrypt',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PKDecrypt",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'PKComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PKComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'MPC',
-          'isAttr': false,
-          'type': 'String'
+          "name": "MPC",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SKEncrypt',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SKEncrypt",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SKDecrypt',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SKDecrypt",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SKComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SKComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SSSharing',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SSSharing",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SSComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SSComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SSReconstruction',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SSReconstruction",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'AddSSSharing',
-          'isAttr': false,
-          'type': 'String'
+          "name": "AddSSSharing",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'AddSSComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "AddSSComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'AddSSReconstruction',
-          'isAttr': false,
-          'type': 'String'
+          "name": "AddSSReconstruction",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'FunSSSharing',
-          'isAttr': false,
-          'type': 'String'
+          "name": "FunSSSharing",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'FunSSComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "FunSSComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'FunSSReconstruction',
-          'isAttr': false,
-          'type': 'String'
+          "name": "FunSSReconstruction",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXProtect',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXProtect",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXAttestationEnclave',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXAttestationEnclave",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXAttestationChallenge',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXAttestationChallenge",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXQuoting',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXQuoting",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'SGXQuoteVerification',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SGXQuoteVerification",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'DimensionalityReduction',
-          'isAttr': false,
-          'type': 'String'
+          "name": "DimensionalityReduction",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'GCGarble',
-          'isAttr': false,
-          'type': 'String'
+          "name": "GCGarble",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'GCEvaluate',
-          'isAttr': false,
-          'type': 'String'
+          "name": "GCEvaluate",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'GCComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "GCComputation",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'OTSend',
-          'isAttr': false,
-          'type': 'String'
+          "name": "OTSend",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'OTReceive',
-          'isAttr': false,
-          'type': 'String'
+          "name": "OTReceive",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'DifferentialPrivacy',
-          'isAttr': false,
-          'type': 'String'
+          "name": "DifferentialPrivacy",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'ProtectConfidentiality',
-          'isAttr': false,
-          'type': 'String'
+          "name": "ProtectConfidentiality",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'OpenConfidentiality',
-          'isAttr': false,
-          'type': 'String'
+          "name": "OpenConfidentiality",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'PETComputation',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PETComputation",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'SQLDerivativeSensitivityTask',
+      name: "SQLDerivativeSensitivityTask",
       extends: [
-        'bpmn:Task'
+        "bpmn:Task"
       ],
       properties: [
         {
-          'name': 'sqlTaskInfo',
-          'isAttr': false,
-          'type': 'String'
+          "name": "sqlTaskInfo",
+          "isAttr": false,
+          "type": "String"
         },
       ]
     },
     {
-      name: 'SQLDataObjectReference',
+      name: "SQLDataObjectReference",
       extends: [
-        'bpmn:DataObjectReference',
-        'bpmn:DataStoreReference',
+        "bpmn:DataObjectReference",
+        "bpmn:DataStoreReference",
       ],
       properties: [
         {
-          'name': 'sqlScript',
-          'isAttr': false,
-          'type': 'String'
+          "name": "sqlScript",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'tableData',
-          'isAttr': false,
-          'type': 'String'
+          "name": "tableData",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'attackerSettings',
-          'isAttr': false,
-          'type': 'String'
+          "name": "isPropagated",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'policyScript',
-          'isAttr': false,
-          'type': 'String'
+          "name": "attackerSettings",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "policyScript",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'StereotypeMessageFlow',
+      name: "StereotypeMessageFlow",
       extends: [
-        'bpmn:MessageFlow'
+        "bpmn:MessageFlow"
       ],
       properties: [
         {
-          'name': 'SecureChannel',
-          'isAttr': false,
-          'type': 'String'
+          "name": "SecureChannel",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'CommunicationProtection',
-          'isAttr': false,
-          'type': 'String'
+          "name": "CommunicationProtection",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'StereotypeDataObjectReference',
+      name: "StereotypeDataObjectReference",
       extends: [
-        'bpmn:DataObjectReference',
-        'bpmn:DataStoreReference'
+        "bpmn:DataObjectReference",
+        "bpmn:DataStoreReference"
       ],
       properties: [
         {
-          'name': 'PKPublic',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PKPublic",
+          "isAttr": false,
+          "type": "String"
         },
         {
-          'name': 'PKPrivate',
-          'isAttr': false,
-          'type': 'String'
+          "name": "PKPrivate",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'SQLDerivativeSensitivityDataObjectReference',
+      name: "SQLDerivativeSensitivityDataObjectReference",
       extends: [
-        'bpmn:DataObjectReference',
-        'bpmn:DataStoreReference'
+        "bpmn:DataObjectReference",
+        "bpmn:DataStoreReference"
       ],
       properties: [
         {
-          'name': 'sqlDataObjectInfo',
-          'isAttr': false,
-          'type': 'String'
+          "name": "sqlDataObjectInfo",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     },
     {
-      name: 'PolicyProcess',
+      name: "PolicyProcess",
       extends: [
-        'bpmn:Process'
+        "bpmn:Process"
       ],
       properties: [
         {
-          'name': 'policyInfo',
-          'isAttr': false,
-          'type': 'String'
+          "name": "policyInfo",
+          "isAttr": false,
+          "type": "String"
         }
       ]
     }
